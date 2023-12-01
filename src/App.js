09c4a7a32427515947f9fd598fd86657cbadaf73
ebukizy1 <ebukizy1@gmail.com>
2023-12-01 11:42:21 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 // import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import { Navbar } from './component/navbar/Navbar';
 import HomePage from './page/homepage/HomePage';
+import Login from './page/login/Login';
+import Signup from './page/sigin/Signup';
 
 function App() {
   return (
+     
     <div className="App">
-        {/* <Navbar /> */}
-        <HomePage />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='login' element={<Login/>} />
+        <Route path='sign' element={ <Signup />} />
+
+
+        </Routes>
+        
 
     
     </div>
--- src/page/homepage/HomePage.jsx
+++ src/page/homepage/HomePage.jsx
@@ -1,18 +1,20 @@
-
-import { Navbar } from '../../component/navbar/Navbar'
-import HeroCard from './card/HeroCard'
-import CreatorCard from './creatorcard/CreatorCard'
-import OngoingProject from './activeproject/OngoingProject'
+import { Navbar } from "../../component/navbar/Navbar";
+import HeroCard from "./card/HeroCard";
+import CreatorCard from "./creatorcard/CreatorCard";
+import OngoingProject from "./activeproject/OngoingProject";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
-     <Navbar />
-     <HeroCard />
-     <CreatorCard />
-      <OngoingProject />
-    </div>
-  )
-}
+  
+      <div>
+        {/* <Navbar /> */}
+        <HeroCard />
+        <CreatorCard />
+        <OngoingProject />
+      </div>
+ 
+  );
+};
 
-export default HomePage
+export default HomePage;
