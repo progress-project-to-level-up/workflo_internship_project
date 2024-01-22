ecc8f269bf26aa35c492920cddcf973d3a90e07f
Vegetablegito247 <Yoshimitsu247Kakarot@gmail.com>
2024-01-22 08:59:14 -0600

    signup, sign in and onboard page desktop design

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-// import './App.css';
 import { Route, Routes } from "react-router-dom";
-import { Navbar } from "./component/navbar/Navbar";
 import HomePage from "./page/homepage/HomePage";
 import Login from "./page/login/Login";
 import Signup from "./page/sigin/Signup";
-// import Onboarding from "./page/onboarding/Onboarding";
-// import VisionHeroCard from "./page/homepage/visionherocard/VisionHeroCard";
 import ProjectList from "./page/projectlist/ProjectList";
+import CreatePageMain from "./page/createProject";
+import OnboardPage from "./page/onboardingPage";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="login" element={<Login />} />
-        <Route path="sign" element={<Signup />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="onboard" element={<OnboardPage />} />
         <Route path="viewAllProject" element={<ProjectList />} />
+        <Route path="create-project" element={<CreatePageMain />} />
       </Routes>
-
-      {/* <Onboarding /> */}
-
-      {/* <VisionHeroCard /> */}
     </div>
   );
 }
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -18,55 +18,53 @@ export const Navbar = () => {
 
   return (
     <>
-      {isHomePage && (
-        <nav className='navbar'>
-          <div className="logAndSearch">
-            <div className="navLogo">
-              <img src={logo} alt="" />
-            </div>
-            <div className="navSearch">
-              <img src={searchIcon} alt="" />
-              <input type="text" placeholder='Search for people, projects..' />
-            </div>
+      <nav className='navbar'>
+        <div className="logAndSearch">
+          <div className="navLogo">
+            <img src={logo} alt="" />
           </div>
+          <div className="navSearch">
+            <img src={searchIcon} alt="" />
+            <input type="text" placeholder='Search for people, projects..' />
+          </div>
+        </div>
 
-          <div className={openSidebar ? "navList active" : "navList"}>
-            <ul className="contList">
-              <li>
-                <NavLink to='/'>
-                  <span>Home</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to='/projects'>
-                  <span>Projects</span>
-                  <img src={dropDown} alt="" />
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to='/discover'>
-                  <span>Discover</span>
-                  <img src={dropDown} alt="" />
-                </NavLink>
-              </li>
-            </ul>
-            <div className="listBtns">
-              <div className="logBtn">
-                <button>Login</button>
-              </div>
-              <div className="joinPro">
-                <button>Join a Project</button>
-              </div>
+        <div className={openSidebar ? "navList active" : "navList"}>
+          <ul className="contList">
+            <li>
+              <NavLink to='/'>
+                <span>Home</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/projects'>
+                <span>Projects</span>
+                <img src={dropDown} alt="" />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/discover'>
+                <span>Discover</span>
+                <img src={dropDown} alt="" />
+              </NavLink>
+            </li>
+          </ul>
+          <div className="listBtns">
+            <div className="logBtn">
+              <button>Login</button>
+            </div>
+            <div className="joinPro">
+              <button>Join a Project</button>
             </div>
           </div>
+        </div>
 
-          <div className="navBurger" onClick={handleSidebar}>
-            <GiHamburgerMenu className='burger' />
-          </div>
+        <div className="navBurger" onClick={handleSidebar}>
+          <GiHamburgerMenu className='burger' />
+        </div>
 
-          <div className={openSidebar ? "placeHolder active" : "placeHolder"} onClick={handleSidebar}></div>
-        </nav>
-      )}
+        <div className={openSidebar ? "placeHolder active" : "placeHolder"} onClick={handleSidebar}></div>
+      </nav>
     </>
   );
 };
