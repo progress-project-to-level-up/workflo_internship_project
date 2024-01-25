b25e65f62e74cf3b04022ad6c686070381cfddcd
Ajayi Ariyo <ariyoaribass68@gmail.com>
2024-01-25 10:40:25 -0500

    my First Push Request

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Signup from "./page/sigin/Signup";
 import ProjectList from "./page/projectlist/ProjectList";
 import CreatePageMain from "./page/createProject";
 import OnboardPage from "./page/onboardingPage";
+import ForgetPassword from "./page/forgetPassword/ForgetPassword";
+import RestPassword from "./page/resetPassword/ResetPassword";
+import VerificationCode from "./page/verificationCode/VerificationCode";
+
 
 function App() {
   return (
@@ -16,6 +20,9 @@ function App() {
         <Route path="onboard" element={<OnboardPage />} />
         <Route path="viewAllProject" element={<ProjectList />} />
         <Route path="create-project" element={<CreatePageMain />} />
+        <Route path="forgetPassword" element={<ForgetPassword />} />
+        <Route path="restPassword" element={<RestPassword/>} />
+        <Route path="verification" element={<VerificationCode />} />
       </Routes>
     </div>
   );
